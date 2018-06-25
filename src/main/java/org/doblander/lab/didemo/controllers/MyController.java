9ab4b958922fc2b3d7bc8dab421c46b2e5507fe8
Intruder <intruder1912@gmail.com>
2018-06-25 21:09:14 +0200

    refactored mycontroller to use di... added another greetingservice implementation as a primary...

--- src/main/java/org/doblander/lab/didemo/controllers/MyController.java
+++ src/main/java/org/doblander/lab/didemo/controllers/MyController.java
@@ -1,13 +1,21 @@
 package org.doblander.lab.didemo.controllers;
 
+import org.doblander.lab.didemo.services.GreetingService;
+import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class MyController {
 
+    private GreetingService greetingService;
+
+    public MyController(GreetingService greetingService) {
+        this.greetingService = greetingService;
+    }
+
     public String hello() {
 
-        System.out.println("Hello from my controller!");
+        System.out.println(greetingService.sayGreeting());
 
         return "hello";
     }
---
+++ src/main/java/org/doblander/lab/didemo/services/PrimaryGreetingService.java
@@ -0,0 +1,14 @@
+package org.doblander.lab.didemo.services;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Service;
+
+@Service
+@Primary
+public class PrimaryGreetingService implements GreetingService {
+
+    @Override
+    public String sayGreeting() {
+        return "Hello from Primary Greeting Service!";
+    }
+}
